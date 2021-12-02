5159520a3873b5e1dbf6abbd44c421d46d1dc0d7
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-02 15:38:10 -0500

    Day 1 Puzzle 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val input = Scanner(System.`in`)
+    var isFirst = true
+    var count = 0
+    var previous = 0
+    while (input.hasNext()) {
+        val current = input.nextInt()
+        if (!isFirst && current > previous) {
+            count += 1
+        }
+        isFirst = false
+        previous = current
+    }
+    println(count)
 }
