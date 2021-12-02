253fd78b44936dfe76e13d5e4a2e517c42272cb0
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-02 15:28:44 -0500

    Day 2 Puzzle 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,16 @@ fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     var horizontal = 0
     var vertical = 0
+    var aim = 0
     while (input.hasNext()) {
         when (input.next()) {
-            "forward" -> horizontal += input.nextInt()
-            "down" -> vertical += input.nextInt()
-            "up" -> vertical -= input.nextInt()
+            "forward" -> {
+                val distance = input.nextInt()
+                horizontal += distance
+                vertical += aim * distance
+            }
+            "down" -> aim += input.nextInt()
+            "up" -> aim -= input.nextInt()
         }
     }
     println(horizontal * vertical)
