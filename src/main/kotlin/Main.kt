9ed1cd35dce9a45b8fb64ac3d30e8d13abd6db5f
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-07 00:18:28 -0500

    Day 7 Puzzle 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,70 +1,29 @@
-import java.util.*
+import lib.Input
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val fishes = scanner.nextLine().split(",").map { it.toInt() }
-    println("Initial state: ${fishes.joinToString(",") { it.toString() }}")
-    val school = fishes.toSchool()
-    println("Initial state transformed: [$school]")
-    println(school.after(256).sum())
-}
-
-fun List<Int>.toSchool(): School {
-    val school = arrayOfNulls<Long>(9).map { 0L }.toMutableList()
-    forEach { fish ->
-        school[fish] += 1L
-    }
-    return School(
-        zero = school[0],
-        one = school[1],
-        two = school[2],
-        three = school[3],
-        four = school[4],
-        five = school[5],
-        six = school[6],
-        seven = school[7],
-        eight = school[8],
-    )
-}
-
-data class School(
-    val zero: Long,
-    val one: Long,
-    val two: Long,
-    val three: Long,
-    val four: Long,
-    val five: Long,
-    val six: Long,
-    val seven: Long,
-    val eight: Long,
-) {
-    override fun toString(): String = mutableListOf<String>()
-        .apply {
-            if (zero > 0) add("zero=$zero")
-            if (one > 0) add("one=$one")
-            if (two > 0) add("two=$two")
-            if (three > 0) add("three=$three")
-            if (four > 0) add("four=$four")
-            if (five > 0) add("five=$five")
-            if (six > 0) add("six=$six")
-            if (seven > 0) add("seven=$seven")
-            if (eight > 0) add("eight=$eight")
+    var max = Integer.MIN_VALUE
+    var min = Integer.MAX_VALUE
+    val crabs = Input().getLines().first().split(",").map { it.toInt() }
+    crabs.forEach {
+        if (it > max) {
+            max = it
         }
-        .joinToString(", ") { it }
+        if (it < min) {
+            min = it
+        }
+    }
+    val distances = arrayOfNulls<Long>(max + 1).mapIndexed { currentPosition, _ ->
+        var totalDistance = 0
+        crabs.forEach { position ->
+            totalDistance += kotlin.math.abs(position - currentPosition)
+        }
+        totalDistance
+    }
+    var smallestDistance: Pair<Int, Int> = -1 to Int.MAX_VALUE
+    distances.forEachIndexed { index, i ->
+        if (i < smallestDistance.second) {
+            smallestDistance = index to i
+        }
+    }
+    println(smallestDistance)
 }
-
-fun School.sum(): Long = zero + one + two + three + four + five + six + seven + eight
-
-fun School.after(days: Int, currentDay: Int = 1): School = School(
-    zero = one,
-    one = two,
-    two = three,
-    three = four,
-    four = five,
-    five = six,
-    six = zero + seven,
-    seven = eight,
-    eight = zero
-)
-    .also { school -> println("After $currentDay ${if (currentDay == 1) "Day" else "Days"}: [$school]") }
-    .run { takeUnless { currentDay < days } ?: after(days, currentDay + 1) }
