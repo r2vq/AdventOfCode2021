71622be88de1845d3ac9aa939efc00de9d4933d5
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-06 22:56:16 -0500

    Create Input class for library

---
+++ src/main/kotlin/lib/Input.kt
@@ -0,0 +1,47 @@
+package lib
+
+import java.util.*
+import java.util.regex.Pattern
+
+class Input(private val scanner: Scanner = Scanner(System.`in`)) {
+
+    fun getInts(): List<Int> = generateSequence {
+        if (scanner.hasNextInt()) {
+            scanner.nextInt()
+        } else {
+            null
+        }
+    }
+        .toList()
+        .also { scanner.close() }
+
+    fun getLines(): List<String> = generateSequence {
+        if (scanner.hasNext()) {
+            scanner.nextLine()
+        } else {
+            null
+        }
+    }
+        .toList()
+        .also { scanner.close() }
+
+    fun get(pattern: String): List<String> = generateSequence {
+        if (scanner.hasNext(pattern)) {
+            scanner.next(pattern)
+        } else {
+            null
+        }
+    }
+        .toList()
+        .also { scanner.close() }
+
+    fun get(pattern: Pattern): List<String> = generateSequence {
+        if (scanner.hasNext(pattern)) {
+            scanner.next(pattern)
+        } else {
+            null
+        }
+    }
+        .toList()
+        .also { scanner.close() }
+}
