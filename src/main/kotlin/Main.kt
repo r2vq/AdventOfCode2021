6e0311571e697bcfb42b9a832c7b0df2b5ffa7ea
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-09 01:01:45 -0500

    Day 9 Puzzle 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,26 +13,65 @@ fun main() {
         .asSequence()
         .mapIndexed { rowIndex, row ->
             row.mapIndexed { colIndex, col ->
-                val isLowerThanLeft = row.getOrNull(colIndex - 1)?.isGreaterThan(col) ?: true
-                val isLowerThanTop = lines.getOrNull(rowIndex - 1)?.getOrNull(colIndex)?.isGreaterThan(col) ?: true
-                val isLowerThanRight = row.getOrNull(colIndex + 1)?.isGreaterThan(col) ?: true
-                val isLowerThanBottom = lines.getOrNull(rowIndex + 1)?.getOrNull(colIndex)?.isGreaterThan(col) ?: true
-                Cell(
-                    isLowPoint = isLowerThanLeft && isLowerThanTop && isLowerThanRight && isLowerThanBottom,
-                    height = col
-                )
+                val top = if (rowIndex > 0) col.isLowerThan(lines[rowIndex - 1][colIndex]) else true
+                val left = if (colIndex > 0) col.isLowerThan(row[colIndex - 1]) else true
+                val bottom = if (rowIndex + 1 < lines.size) col.isLowerThan(lines[rowIndex + 1][colIndex]) else true
+                val right = if (colIndex + 1 < row.size) col.isLowerThan(row[colIndex + 1]) else true
+                Cell(left && top && right && bottom, col, rowIndex, colIndex)
             }
         }
-        .flatten()
+    val basins = cells.flatten()
         .filter { it.isLowPoint }
-        .map { it.height + 1 }
-        .reduce { acc, cell -> acc + cell }
-    println(cells)
+        .map { cell -> cell.findNeighbours(cells.toList()) }
+        .map { grid -> grid.map { it.height }.size }
+        .onEach { println(it) }
+        .toList()
+
+    var first = 0
+    var second = 0
+    var third = 0
+
+    basins.forEach { basin ->
+        if (basin > first) {
+            third = second
+            second = first
+            first = basin
+        } else if (basin > second) {
+            third = second
+            second = first
+        } else if (basin > third) {
+            third = basin
+        }
+    }
+
+    println("$first * $second * $third = ${first * second * third}")
 }
 
-fun Int.isGreaterThan(other: Int): Boolean {
-    return this > other
-}
+fun Cell.findNeighbours(grid: List<List<Cell>>): Set<Cell> = listOfNotNull(
+    if (rowIndex > 0 && height < grid[rowIndex - 1][colIndex].height && grid[rowIndex - 1][colIndex].height < 9) {
+        grid[rowIndex - 1][colIndex].findNeighbours(grid)
+    } else {
+        null
+    },
+    if (colIndex > 0 && height < grid[rowIndex][colIndex - 1].height && grid[rowIndex][colIndex - 1].height < 9) {
+        grid[rowIndex][colIndex - 1].findNeighbours(grid)
+    } else {
+        null
+    },
+    if (rowIndex + 1 < grid.size && height < grid[rowIndex + 1][colIndex].height && grid[rowIndex + 1][colIndex].height < 9) {
+        grid[rowIndex + 1][colIndex].findNeighbours(grid)
+    } else {
+        null
+    },
+    if (colIndex + 1 < grid[rowIndex].size && height < grid[rowIndex][colIndex + 1].height && grid[rowIndex][colIndex + 1].height < 9) {
+        grid[rowIndex][colIndex + 1].findNeighbours(grid)
+    } else {
+        null
+    }
+)
+    .flatten()
+    .plus(this)
+    .toSet()
 
 fun Int.isLowerThan(other: Int): Boolean {
     return this < other
@@ -40,5 +79,7 @@ fun Int.isLowerThan(other: Int): Boolean {
 
 data class Cell(
     val isLowPoint: Boolean,
-    val height: Int
+    val height: Int,
+    val rowIndex: Int,
+    val colIndex: Int
 )
