f0d77a76909d89cd18ad07d50e369ca19c234190
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-12 10:07:55 -0500

    Day 12 Puzzle 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,96 +1,85 @@
 import lib.Input
 
-typealias Grid = List<List<Octopus>>
-
-var totalFlashes = 0L
-
 fun main() {
-    val numbers = Input()
-        .getLines()
-        .map { line ->
-            line
-                .split("")
-                .mapNotNull { char -> char.takeIf { it.isNotEmpty() } }
-                .map { char -> char.toInt().let { Octopus(it) } }
-        }
-
-    numbers.draw()
-    numbers.takeSteps().draw()
-    println("Total Flashes: $totalFlashes")
+    val nodes = Input().getLines().map { it.split("-") }.createNodes()
+    val startNode = nodes.getValue("start")
+    val paths = nodes.travel(startNode, setOf(startNode.name))
+    println(paths)
 }
 
-fun Grid.takeSteps(count: Int = 0): Grid {
-    println("step: $count")
-    return takeIf {
-        var allZero = true
-        forEach { row -> row.forEach { octopus -> if (octopus.energy != 0) allZero = false } }
-        allZero
-    } ?: step().flash().clear().takeSteps(count + 1)
-}
-
-//fun Grid.takeSteps(count: Int): Grid = takeIf { count == 0 } ?: step().flash().clear().takeSteps(count - 1)
-
-fun Grid.step(): Grid = map { row -> row.map { octopus -> octopus.copy(energy = octopus.energy + 1) } }
-
-fun Grid.flash(): Grid {
-    val flashed = mutableListOf<Pair<Int, Int>>()
-    val result: List<List<Octopus>> = mapIndexed { i, row ->
-        row.mapIndexed { j, octopus ->
-            if (octopus.energy > 9) {
-                totalFlashes += 1
-                flashed.add(i to j)
-                Octopus(0, true)
+fun Map<String, Node>.travel(node: Node, expired: Set<String>): Int = if (node.name == "end") {
+    1
+} else {
+    node
+        .connections
+        .map { connection ->
+            if (!expired.contains(connection)) {
+                val connectionNode = getValue(connection)
+                val childExpired = expired.takeUnless { !connectionNode.isLarge } ?: expired.plus(connection)
+                travel(connectionNode, childExpired)
             } else {
-                octopus
+                0
             }
         }
+        .reduce { acc, i -> acc + i }
+}
+
+fun List<List<String>>.createNodes(): Map<String, Node> = fold(mutableMapOf()) { nodes, pair ->
+    val (firstName, secondName) = pair
+    val first = nodes.lazyAddIfNew(firstName) {
+        Node(
+            name = firstName,
+            connections = emptySet(),
+            isLarge = firstName.isAllUpperCase()
+        )
     }
-    return if (flashed.isNotEmpty()) {
-        var map = result
-        flashed.forEach { (rowIndex, colIndex) ->
-            map = map.mapIndexed { i, row ->
-                row.mapIndexed { j, cell ->
-                    when {
-                        (i - 1 == rowIndex && j - 1 == colIndex) ||
-                                (i - 1 == rowIndex && j == colIndex) ||
-                                (i - 1 == rowIndex && j + 1 == colIndex) ||
-                                (i == rowIndex && j - 1 == colIndex) ||
-                                (i == rowIndex && j + 1 == colIndex) ||
-                                (i + 1 == rowIndex && j - 1 == colIndex) ||
-                                (i + 1 == rowIndex && j == colIndex) ||
-                                (i + 1 == rowIndex && j + 1 == colIndex) -> {
-                            cell.copy(energy = cell.energy + 1)
-                        }
-                        else -> cell
-                    }
-                }
-            }
-        }
-        map.flash().mapIndexed { i, row ->
-            row.mapIndexed { j, cell ->
-                if (flashed.contains(i to j)) {
-                    cell.copy(energy = 0)
-                } else {
-                    cell
-                }
-            }
-        }
-    } else {
-        result
+    val second = nodes.lazyAddIfNew(secondName) {
+        Node(
+            name = secondName,
+            connections = emptySet(),
+            isLarge = secondName.isAllUpperCase()
+        )
+    }
+    nodes.transform(firstName) {
+        copy(
+            connections = connections.plus(second.name)
+        )
+    }
+    nodes.transform(secondName) {
+        copy(
+            connections = connections.plus(first.name)
+        )
     }
 }
 
-fun Grid.clear(): Grid = map { row -> row.map { octopus -> octopus.copy(didFlash = false) } }
-
-fun Grid.draw(): Grid {
-    println("=========================================")
-    map { row -> row.map { String.format("%2d", it.energy) } }
-        .forEach { row -> println(row) }
-    println("=========================================")
-    return this
+fun <T, U> MutableMap<T, U>.lazyAddIfNew(key: T, block: () -> U): U {
+    val value = block()
+    if (!contains(key)) {
+        put(key, value)
+    }
+    return value
 }
 
-data class Octopus(
-    val energy: Int,
-    val didFlash: Boolean = false,
-)
+fun <T, U> MutableMap<T, U>.transform(key: T, transform: U.() -> U): MutableMap<T, U> =
+    this.also { set(key, getValue(key).transform()) }
+
+fun String.isAllUpperCase(): Boolean {
+    forEach {
+        if (!it.isUpperCase()) {
+            return false
+        }
+    }
+    return true
+}
+
+data class Node(
+    val name: String,
+    val connections: Set<String>,
+    val isLarge: Boolean,
+) {
+    override fun toString(): String {
+        val connectionNames =
+            connections.fold(listOf<String>()) { acc, node -> acc + node }.joinToString(", ") { it }
+        return "Node(name=$name, connections=$connectionNames, isLarge=$isLarge)"
+    }
+}
