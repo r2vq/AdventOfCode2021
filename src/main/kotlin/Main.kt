e8dc0a59a41a0b545faf99aa6e51577e7f2b6252
Keane Quibilan <arv.r2vq@gmail.com>
2021-12-12 11:09:40 -0500

    Day 12 Puzzle 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,85 +1,73 @@
 import lib.Input
 
+private const val NODE_START = "start"
+private const val NODE_END = "end"
+
 fun main() {
     val nodes = Input().getLines().map { it.split("-") }.createNodes()
-    val startNode = nodes.getValue("start")
-    val paths = nodes.travel(startNode, setOf(startNode.name))
-    println(paths)
+    val startNode = nodes.getValue(NODE_START)
+    val paths = nodes.filter { node -> node.key != NODE_START && node.key != NODE_END && !node.value.isLarge }
+        .flatMap { exception -> mapOf(startNode.name to 1).traverse(nodes, startNode, exception.value, startNode.name) }
+        .toSet()
+
+    println(paths.size)
 }
 
-fun Map<String, Node>.travel(node: Node, expired: Set<String>): Int = if (node.name == "end") {
-    1
-} else {
-    node
-        .connections
-        .map { connection ->
-            if (!expired.contains(connection)) {
-                val connectionNode = getValue(connection)
-                val childExpired = expired.takeUnless { !connectionNode.isLarge } ?: expired.plus(connection)
-                travel(connectionNode, childExpired)
-            } else {
-                0
+fun List<List<String>>.createNodes(): Map<String, Node> = fold(mapOf()) { nodes, (firstName, secondName) ->
+    nodes
+        .lazyPlusIfNotExists(firstName) {
+            Node(
+                name = firstName,
+                connections = emptySet(),
+                isLarge = firstName.isAllUpperCase()
+            )
+        }
+        .lazyPlusIfNotExists(secondName) {
+            Node(
+                name = secondName,
+                connections = emptySet(),
+                isLarge = secondName.isAllUpperCase()
+            )
+        }
+        .update(firstName) { copy(connections = connections.plus(secondName)) }
+        .update(secondName) { copy(connections = connections.plus(firstName)) }
+}
+
+fun Map<String, Int>.traverse(nodes: Map<String, Node>, node: Node, exception: Node, path: String): List<String?> =
+    if (node.name == NODE_END) {
+        listOf(path)
+    } else {
+        node.connections.mapNotNull { connectionName ->
+            val connection = nodes.getValue(connectionName)
+            when {
+                connection.isLarge || !contains(connectionName) -> plus(connectionName to 0)
+                    .traverse(
+                        nodes = nodes,
+                        node = connection,
+                        exception = exception,
+                        path = "$path,$connectionName"
+                    )
+                connectionName == exception.name && get(exception.name) == 0 -> plus(connectionName to 1)
+                    .traverse(
+                        nodes = nodes,
+                        node = connection,
+                        exception = exception,
+                        path = "$path,$connectionName"
+                    )
+                else -> null
             }
-        }
-        .reduce { acc, i -> acc + i }
-}
+        }.flatten()
+    }
 
-fun List<List<String>>.createNodes(): Map<String, Node> = fold(mutableMapOf()) { nodes, pair ->
-    val (firstName, secondName) = pair
-    val first = nodes.lazyAddIfNew(firstName) {
-        Node(
-            name = firstName,
-            connections = emptySet(),
-            isLarge = firstName.isAllUpperCase()
-        )
-    }
-    val second = nodes.lazyAddIfNew(secondName) {
-        Node(
-            name = secondName,
-            connections = emptySet(),
-            isLarge = secondName.isAllUpperCase()
-        )
-    }
-    nodes.transform(firstName) {
-        copy(
-            connections = connections.plus(second.name)
-        )
-    }
-    nodes.transform(secondName) {
-        copy(
-            connections = connections.plus(first.name)
-        )
-    }
-}
+fun <T, U> Map<T, U>.lazyPlusIfNotExists(key: T, initializer: () -> U): Map<T, U> = takeUnless { !it.contains(key) }
+    ?: plus(key to initializer())
 
-fun <T, U> MutableMap<T, U>.lazyAddIfNew(key: T, block: () -> U): U {
-    val value = block()
-    if (!contains(key)) {
-        put(key, value)
-    }
-    return value
-}
+fun <T, U> Map<T, U>.update(key: T, transformer: U.() -> U): Map<T, U> = plus(key to getValue(key).transformer())
 
-fun <T, U> MutableMap<T, U>.transform(key: T, transform: U.() -> U): MutableMap<T, U> =
-    this.also { set(key, getValue(key).transform()) }
-
-fun String.isAllUpperCase(): Boolean {
-    forEach {
-        if (!it.isUpperCase()) {
-            return false
-        }
-    }
-    return true
-}
+fun String.isAllUpperCase(): Boolean = this == uppercase()
 
 data class Node(
     val name: String,
     val connections: Set<String>,
     val isLarge: Boolean,
-) {
-    override fun toString(): String {
-        val connectionNames =
-            connections.fold(listOf<String>()) { acc, node -> acc + node }.joinToString(", ") { it }
-        return "Node(name=$name, connections=$connectionNames, isLarge=$isLarge)"
-    }
-}
+)
